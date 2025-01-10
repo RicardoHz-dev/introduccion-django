11317ea0acc524a2345eb8b6537219f97db7a269
RicardoHz-dev <172085931+RicardoHz-dev@users.noreply.github.com>
2025-01-10 17:37:06 -0500

    crear un post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, BlogCreateView
 app_name = 'blog'
 
 
 # ACCEDER A TODAS LAS VISTAS QUE CREAREMOS DEL BLOG
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home')
+    path('', BlogListView.as_view(), name='home'),
+    path('create/', BlogCreateView.as_view(), name='create')
 ]
 
